9c0f840902558ced6a0ddd82b37827bd5754e2e6
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-03-23 13:12:29 +0300

    Update Task2.js

--- day1/Task2.js
+++ day1/Task2.js
@@ -1,7 +1,9 @@
-function arrayFirstLettersFromWords(string){
+function insertFirstLettersFromWords(string){
    let newString = string.split(' ').map( item => item[0]);
    return newString.filter(word => word.length > 0);
 }
 
+
 let result = arrayFirstLettersFromWords('Каждый охотник желает знать где сидит фазан');
 console.log(result);
+
